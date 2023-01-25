b8b50720c524dc66d066d9740aa06dd7b93dcfd9
NickMoo5 <nicholasamah@gmail.com>
2023-01-24 22:42:14 -0800

    implement array iterator

--- hw1-cmpt225-spring23/src/integeriterators/ArrayIterator.java
+++ hw1-cmpt225-spring23/src/integeriterators/ArrayIterator.java
@@ -1,13 +1,21 @@
 package integeriterators;
 
+import java.util.NoSuchElementException;
+
 public class ArrayIterator implements IntegerIterator
 {
+
+	protected boolean IS_CIRCULAR;
+	protected int[] currArray;
+	protected int currIndex;
 
 	/**
 	 * Creates an iterator for ar 
 	 */
 	public ArrayIterator(int[] ar) {
-		// TODO implement me
+		IS_CIRCULAR = false;
+		currArray = ar;
+		currIndex = 0;
 	}
 	
 	/**
@@ -17,22 +25,39 @@ public class ArrayIterator implements IntegerIterator
 	 * ar[0],ar[1]...ar[ar.length-1],ar[0],ar[1]...ar[ar.length-1],ar[0]...
 	 */
 	public ArrayIterator(int[] ar, boolean isCircular) {
-		// TODO implement me
+		IS_CIRCULAR = isCircular;
+		currArray = ar;
+		currIndex = 0;
 	}
 	
 	@Override
 	public boolean hasNext() {
-		// TODO implement me
-		return false;
+		if (currIndex > currArray.length - 1 && !IS_CIRCULAR) {
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
-	public Integer next() {
-		// TODO implement me
-		return -1;
+	public Integer next() throws NoSuchElementException {
+		int returnIndex = currIndex;
+
+		if (IS_CIRCULAR) {
+			if (currIndex == currArray.length) {
+				this.reset();
+				returnIndex = currIndex;
+			}
+		} else {
+			if (!hasNext()) {
+				NoSuchElementException ex = new NoSuchElementException("No next element");
+				throw ex;
+			}
+		}
+		currIndex += 1;
+		return currArray[returnIndex];
 	}
 	
 	public void reset() {
-		// TODO implement me
+		currIndex = 0;
 	}
 }
