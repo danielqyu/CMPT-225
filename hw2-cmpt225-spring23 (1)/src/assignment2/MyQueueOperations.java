94dc13e5105bb1d54919ae68240ae2287463e105
Nicholas Mah <nicholasamah@gmail.com>
2023-02-06 22:38:24 -0800

    implement queue operations

--- hw2-cmpt225-spring23 (1)/src/assignment2/MyQueueOperations.java
+++ hw2-cmpt225-spring23 (1)/src/assignment2/MyQueueOperations.java
@@ -1,14 +1,21 @@
 package assignment2;
 
 import basicdatastructures.queue.*;
+import basicdatastructures.stack.Stack;
+import basicdatastructures.stack.StackLinkedListBased;
 
 public class MyQueueOperations {
 	/**
 	 * Returns the number of elements in q.
 	 */
 	public static <T> int size(Queue<T> q) {
-		// TODO implement me
-		return -1;
+		Queue<T> qCopy = MyQueueOperations.clone(q);
+		int counter = 0;
+		while (!qCopy.isEmpty()) {
+			qCopy.dequeue();
+			counter++;
+		}
+		return counter;
 	}
 
 	/**
@@ -17,15 +24,32 @@ public class MyQueueOperations {
 	 * QueueArrayBased or QueueLinkedListBased, up to you.
 	 */
 	public static <T> Queue<T> clone(Queue<T> orig) {
-		// TODO implement me
-		return null;
+		T item;
+		Queue<T> qCopy = new QueueLinkedListBased<>();
+		Queue<T> og = new QueueLinkedListBased<>();
+		while (!orig.isEmpty()) {
+			item = orig.dequeue();
+			qCopy.enqueue(item);
+			og.enqueue(item);
+		}
+		while (!og.isEmpty()) {
+			item = og.dequeue();
+			orig.enqueue(item);
+		}
+		return qCopy;
 	}
 
 	/**
 	 * Reverses the order of the elements in q.
 	 */
 	public static <T> void reverse(Queue<T> q) {
-		// TODO implement me
+		Stack<T> bufferStack = new StackLinkedListBased<T>();
+		while (!q.isEmpty()) {
+			bufferStack.push(q.dequeue());
+		}
+		while (!bufferStack.isEmpty()) {
+			q.enqueue(bufferStack.pop());
+		}
 	}
 
 	/**
@@ -33,7 +57,21 @@ public class MyQueueOperations {
 	 * the queues are compared using == operator.
 	 */
 	public static <T> boolean areEqual(Queue<T> q1, Queue<T> q2) {
-		// TODO implement me
-		return false;
+		// make copies
+		Queue<T> buffQ1 = MyQueueOperations.clone(q1);
+		Queue<T> buffQ2 = MyQueueOperations.clone(q2);
+
+		while (!buffQ1.isEmpty() || !buffQ2.isEmpty()) {
+			if (buffQ1.isEmpty() != buffQ2.isEmpty()) {
+				return false;
+			}
+			if (buffQ1.dequeue() != buffQ2.dequeue()) {
+				return false;
+			}
+		}
+
+
+
+		return true;
 	}
 }
